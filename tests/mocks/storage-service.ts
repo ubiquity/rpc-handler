f72907e847301a4e18e7c8bc32dae681d6f583da
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-05-20 21:03:58 +0100

    chore: mocks

---
+++ tests/mocks/storage-service.ts
@@ -0,0 +1,56 @@
+export class StorageService {
+  static getLatencies(env: string, networkId: number): Record<string | number, number> {
+    if (env === "browser") {
+      if (typeof localStorage === "undefined") {
+        console.log("Passing because localStorage is not available");
+        return {};
+      }
+      const latencies: Record<string, number> = JSON.parse(localStorage.getItem("rpcLatencies") || "{}");
+      return Object.keys(latencies).reduce((acc: Record<string, number>, key) => {
+        if (key.startsWith(`${networkId}__`)) {
+          acc[key] = latencies[key];
+        }
+        return acc;
+      }, {});
+    }
+
+    return {};
+  }
+
+  static getRefreshLatencies(env: string): number {
+    if (env === "browser") {
+      if (typeof localStorage === "undefined") {
+        console.log("Passing because localStorage is not available");
+        return 0;
+      }
+      const refresh = JSON.parse(localStorage.getItem("refreshLatencies") || "0");
+
+      if (typeof refresh === "number") {
+        return refresh;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  }
+
+  static setLatencies(env: string, latencies: Record<string | number, number>): void {
+    if (env === "browser") {
+      if (typeof localStorage === "undefined") {
+        console.log("Passing because localStorage is not available");
+        return;
+      }
+      localStorage.setItem("rpcLatencies", JSON.stringify(latencies));
+    }
+  }
+
+  static setRefreshLatencies(env: string, refreshLatencies: number): void {
+    if (env === "browser") {
+      if (typeof localStorage === "undefined") {
+        console.log("Passing because localStorage is not available");
+        return;
+      }
+      localStorage.setItem("refreshLatencies", JSON.stringify(refreshLatencies));
+    }
+  }
+}
