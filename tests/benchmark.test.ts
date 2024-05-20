702f62f48a89107571e92e3313cbfafd780aee16
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-05-20 21:04:20 +0100

    chore: updated tests

---
+++ tests/benchmark.test.ts
@@ -0,0 +1,56 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { HandlerConstructorConfig } from "../dist/tests/mocks/handler";
+import { RPCHandler } from "../dist/tests/mocks/rpc-handler";
+
+export const testConfig: HandlerConstructorConfig = {
+  networkId: 1,
+  autoStorage: false,
+  cacheRefreshCycles: 3,
+  networkName: null,
+  networkRpcs: null,
+  rpcTimeout: 1500,
+  runtimeRpcs: null,
+};
+
+describe("RPCHandler", () => {
+  let provider: JsonRpcProvider;
+  let rpcHandler: RPCHandler;
+
+  beforeEach(async () => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
+
+  describe("benchmark", () => {
+    it("executing without an rpc timeout", async () => {
+      rpcHandler = new RPCHandler({
+        ...testConfig,
+        rpcTimeout: 99999999,
+      });
+      await rpcHandler.testRpcPerformance();
+      const latencies = rpcHandler.getLatencies();
+
+      const slowestRpc = latencies[Object.keys(latencies).sort((a, b) => latencies[b] - latencies[a])[0]];
+      const fastestRpcUrl = latencies[Object.keys(latencies).sort((a, b) => latencies[a] - latencies[b])[0]];
+
+      console.log("slowestRpc: without an rpc timeout > ", slowestRpc);
+      console.log("fastestRpc: without an rpc timeout > ", fastestRpcUrl);
+    }, 999999);
+
+    it("executing with an rpc timeout", async () => {
+      rpcHandler = new RPCHandler({
+        ...testConfig,
+        rpcTimeout: 1500,
+      });
+      await rpcHandler.testRpcPerformance();
+      const latencies = rpcHandler.getLatencies();
+
+      const slowestRpc = latencies[Object.keys(latencies).sort((a, b) => latencies[b] - latencies[a])[0]];
+      const fastestRpcUrl = latencies[Object.keys(latencies).sort((a, b) => latencies[a] - latencies[b])[0]];
+
+      console.log("slowestRpc: with an rpc timeout > ", slowestRpc);
+      console.log("fastestRpc: with an rpc timeout > ", fastestRpcUrl);
+    }, 999999);
+  });
+});
