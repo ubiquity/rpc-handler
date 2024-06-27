Keyrxng/web3-rpc-racer-1

refactor: move services into /types

types/rpc-service.ts
@@ -1,0 +1,100 @@
+import { ValidBlockData } from "./handler";
+import axios from "axios";
+type PromiseResult = { success: boolean; rpcUrl: string; duration: number };
+
+export class RPCService {
+  static async testRpcPerformance(
+    networkId: number,
+    latencies: Record<string, number>,
+    runtimeRpcs: string[],
+    rpcHeader: object,
+    rpcBody: string,
+    rpcTimeout: number
+  ): Promise<{ latencies: Record<string, number>; runtimeRpcs: string[] }> {
+    const instance = axios.create({
+      timeout: rpcTimeout,
+      headers: rpcHeader,
+    });
+
+    const timeoutPromise = new Promise((resolve) => {
+      const timeoutId = setTimeout(() => resolve({ success: false, duration: 0 }), rpcTimeout);
+      timeoutId.unref();
+    });
+
+    const successfulPromises = runtimeRpcs.map<Promise<PromiseResult>>(
+      (rpcUrl) =>
+        new Promise<PromiseResult>((resolve) => {
+          const startTime = performance.now();
+          Promise.race([instance.post(rpcUrl, rpcBody), timeoutPromise])
+            .then(() => {
+              resolve({
+                rpcUrl,
+                duration: performance.now() - startTime,
+                success: true,
+              });
+            })
+            .catch(() => {
+              resolve({ rpcUrl, success: false, duration: 0 });
+            });
+        })
+    );
+
+    const fastest = await Promise.race(successfulPromises);
+
+    if (fastest.success) {
+      latencies[`${networkId}__${fastest.rpcUrl}`] = fastest.duration;
+    }
+
+    const allResults = await Promise.allSettled(successfulPromises);
+
+    allResults.forEach((result) => {
+      if (result.status === "fulfilled" && result.value.success) {
+        latencies[`${networkId}__${result.value.rpcUrl}`] = result.value.duration;
+      } else if (result.status === "fulfilled") {
+        const fulfilledResult = result.value;
+        const index = runtimeRpcs.indexOf(fulfilledResult.rpcUrl);
+        if (index > -1) {
+          runtimeRpcs.splice(index, 1);
+        }
+      }
+    });
+
+    return { latencies, runtimeRpcs };
+  }
+
+  static async findFastestRpc(latencies: Record<string, number>, networkId: number): Promise<string | null> {
+    if (Object.keys(latencies).length === 0) {
+      throw new Error("[RPCService] No latencies found");
+    }
+    try {
+      const validLatencies: Record<string, number> = Object.entries(latencies)
+        .filter(([key]) => key.startsWith(`${networkId}__`))
+        .reduce(
+          (acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          },
+          {} as Record<string, number>
+        );
+
+      return Object.keys(validLatencies)
+        .reduce((a, b) => (validLatencies[a] < validLatencies[b] ? a : b))
+        .split("__")[1];
+    } catch (error) {
+      console.error("[RPCService] Failed to find fastest RPC", error);
+      return null;
+    }
+  }
+
+  static _verifyBlock(data: ValidBlockData): boolean {
+    try {
+      const { jsonrpc, id, result } = data;
+      const { number, timestamp, hash } = result;
+      return (
+        jsonrpc === "2.0" && id === 1 && parseInt(number, 16) > 0 && parseInt(timestamp, 16) > 0 && hash.match(/[0-9|a-f|A-F|x]/gm)?.join("").length === 66
+      );
+    } catch (error) {
+      return false;
+    }
+  }
+}

types/storage-service.ts
@@ -1,0 +1,50 @@
+export class StorageService {
+  static getLatencies(env: string, networkId: number): Record<string | number, number> {
+    if (env === "browser") {
+      if (this.bypassForTests()) return {};
+      const latencies: Record<string, number> = JSON.parse(localStorage.getItem("rpcLatencies") || "{}");
+      return Object.keys(latencies).reduce((acc: Record<string, number>, key) => {
+        if (key.startsWith(`${networkId}__`)) {
+          acc[key] = latencies[key];
+        }
+        return acc;
+      }, {});
+    }
+
+    return {};
+  }
+
+  static getRefreshLatencies(env: string): number {
+    if (env === "browser") {
+      if (this.bypassForTests()) return 0;
+      const refresh = JSON.parse(localStorage.getItem("refreshLatencies") || "0");
+
+      if (typeof refresh === "number") {
+        return refresh;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  }
+  static setLatencies(env: string, latencies: Record<string | number, number>): void {
+    if (env === "browser") {
+      if (this.bypassForTests()) return;
+      localStorage.setItem("rpcLatencies", JSON.stringify(latencies));
+    }
+  }
+  static setRefreshLatencies(env: string, refreshLatencies: number): void {
+    if (env === "browser") {
+      if (this.bypassForTests()) return;
+      localStorage.setItem("refreshLatencies", JSON.stringify(refreshLatencies));
+    }
+  }
+
+  // This method is only used for env detection testing
+  static bypassForTests() {
+    if (typeof localStorage === "undefined") {
+      console.log("Passing test because localStorage is not defined.");
+      return true;
+    }
+  }
+}

ubiquity/rpc-handler

types/rpc-service.ts
@@ -1,0 +1,100 @@
+import { NetworkId, ValidBlockData } from "./handler";
+import axios from "axios";
+type PromiseResult = { success: boolean; rpcUrl: string; duration: number };
+
+export class RPCService {
+  static async testRpcPerformance(
+    networkId: NetworkId,
+    latencies: Record<string, number>,
+    runtimeRpcs: string[],
+    rpcHeader: object,
+    rpcBody: string,
+    rpcTimeout: number
+  ): Promise<{ latencies: Record<string, number>; runtimeRpcs: string[] }> {
+    const instance = axios.create({
+      timeout: rpcTimeout,
+      headers: rpcHeader,
+    });
+
+    const timeoutPromise = new Promise((resolve) => {
+      const timeoutId = setTimeout(() => resolve({ success: false, duration: 0 }), rpcTimeout);
+      timeoutId.unref();
+    });
+
+    const successfulPromises = runtimeRpcs.map<Promise<PromiseResult>>(
+      (rpcUrl) =>
+        new Promise<PromiseResult>((resolve) => {
+          const startTime = performance.now();
+          Promise.race([instance.post(rpcUrl, rpcBody), timeoutPromise])
+            .then(() => {
+              resolve({
+                rpcUrl,
+                duration: performance.now() - startTime,
+                success: true,
+              });
+            })
+            .catch(() => {
+              resolve({ rpcUrl, success: false, duration: 0 });
+            });
+        })
+    );
+
+    const fastest = await Promise.race(successfulPromises);
+
+    if (fastest.success) {
+      latencies[`${networkId}__${fastest.rpcUrl}`] = fastest.duration;
+    }
+
+    const allResults = await Promise.allSettled(successfulPromises);
+
+    allResults.forEach((result) => {
+      if (result.status === "fulfilled" && result.value.success) {
+        latencies[`${networkId}__${result.value.rpcUrl}`] = result.value.duration;
+      } else if (result.status === "fulfilled") {
+        const fulfilledResult = result.value;
+        const index = runtimeRpcs.indexOf(fulfilledResult.rpcUrl);
+        if (index > -1) {
+          runtimeRpcs.splice(index, 1);
+        }
+      }
+    });
+
+    return { latencies, runtimeRpcs };
+  }
+
+  static async findFastestRpc(latencies: Record<string, number>, networkId: NetworkId): Promise<string | null> {
+    if (Object.keys(latencies).length === 0) {
+      throw new Error("[RPCService] No latencies found");
+    }
+    try {
+      const validLatencies: Record<string, number> = Object.entries(latencies)
+        .filter(([key]) => key.startsWith(`${networkId}__`))
+        .reduce(
+          (acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          },
+          {} as Record<string, number>
+        );
+
+      return Object.keys(validLatencies)
+        .reduce((a, b) => (validLatencies[a] < validLatencies[b] ? a : b))
+        .split("__")[1];
+    } catch (error) {
+      console.error("[RPCService] Failed to find fastest RPC", error);
+      return null;
+    }
+  }
+
+  static _verifyBlock(data: ValidBlockData): boolean {
+    try {
+      const { jsonrpc, id, result } = data;
+      const { number, timestamp, hash } = result;
+      return (
+        jsonrpc === "2.0" && id === 1 && parseInt(number, 16) > 0 && parseInt(timestamp, 16) > 0 && hash.match(/[0-9|a-f|A-F|x]/gm)?.join("").length === 66
+      );
+    } catch (error) {
+      return false;
+    }
+  }
+}

types/storage-service.ts
@@ -1,0 +1,52 @@
+import { NetworkId } from "./handler";
+
+export class StorageService {
+  static getLatencies(env: string, networkId: NetworkId): Record<string | number, number> {
+    if (env === "browser") {
+      if (this.bypassForTests()) return {};
+      const latencies: Record<string, number> = JSON.parse(localStorage.getItem("rpcLatencies") || "{}");
+      return Object.keys(latencies).reduce((acc: Record<string, number>, key) => {
+        if (key.startsWith(`${networkId}__`)) {
+          acc[key] = latencies[key];
+        }
+        return acc;
+      }, {});
+    }
+
+    return {};
+  }
+
+  static getRefreshLatencies(env: string): number {
+    if (env === "browser") {
+      if (this.bypassForTests()) return 0;
+      const refresh = JSON.parse(localStorage.getItem("refreshLatencies") || "0");
+
+      if (typeof refresh === "number") {
+        return refresh;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  }
+  static setLatencies(env: string, latencies: Record<string | number, number>): void {
+    if (env === "browser") {
+      if (this.bypassForTests()) return;
+      localStorage.setItem("rpcLatencies", JSON.stringify(latencies));
+    }
+  }
+  static setRefreshLatencies(env: string, refreshLatencies: number): void {
+    if (env === "browser") {
+      if (this.bypassForTests()) return;
+      localStorage.setItem("refreshLatencies", JSON.stringify(refreshLatencies));
+    }
+  }
+
+  // This method is only used for env detection testing
+  static bypassForTests() {
+    if (typeof localStorage === "undefined") {
+      console.log("Passing test because localStorage is not defined.");
+      return true;
+    }
+  }
+}