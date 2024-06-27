Keyrxng/web3-rpc-racer-1

chore: readme and comments

types/handler.ts
@@ -1,6 +1,6 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
 import { ChainId, networkCurrencies, networkExplorers, networkIds, networkNames, networkRpcs, tokens } from "./constants";
-import { PrettyLogs, PrettyLogsWithOk } from "./logs";
+import { LogInterface, PrettyLogs, PrettyLogsWithOk } from "./logs";
 
 export type ValidBlockData = {
   jsonrpc: string;
@@ -30,27 +30,29 @@ export type HandlerInterface = {
   testRpcPerformance(): Promise<JsonRpcProvider | null>;
 };
 
-type ModuleName = "[RPCHandler Provider Proxy] -> ";
+// This is log message prefix which can be used to identify the logs from this module
+type ModuleName = "[RPCHandler Provider Proxy] - ";
 
 type ProxySettings = {
-  retryCount: number;
-  retryDelay: number;
+  retryCount: number; // how many times we'll loop the list of RPCs retrying the request before failing
+  retryDelay: number; // how long we'll wait before moving to the next RPC
   // eslint-disable-next-line @typescript-eslint/ban-types
-  logTier: (PrettyLogsWithOk & {}) | null;
-  logger: PrettyLogs | null;
-  strictLogs: boolean;
-  moduleName?: ModuleName | string;
+  logTier: (PrettyLogsWithOk & {}) | null; // set to "none" for no logs, null will default to "error", "verbose" will log all
+  logger: PrettyLogs | LogInterface | null; // null will default to PrettyLogs, otherwise pass in your own logger
+  strictLogs: boolean; // true is default, only the specified logTier will be logged. false will log all logs.
+  moduleName?: ModuleName | string; // this is the prefix for the logs
+  disabled?: boolean;
 };
 
 export type HandlerConstructorConfig = {
   networkId: number;
-  networkName: string | null;
-  networkRpcs: string[] | null;
-  autoStorage: boolean | null;
-  cacheRefreshCycles: number | null;
-  runtimeRpcs: string[] | null;
-  rpcTimeout: number | null;
-  proxySettings: ProxySettings;
+  networkName: string | null; // will default using the networkRpcs
+  networkRpcs: string[] | null; // e.g "https://mainnet.infura.io/..."
+  autoStorage: boolean | null; // browser only, will store in localStorage
+  cacheRefreshCycles: number | null; // bad RPCs are excluded if they fail, this is how many cycles before they're re-tested
+  runtimeRpcs: string[] | null; // e.g "<networkId>__https://mainnet.infura.io/..." > "1__https://mainnet.infura.io/..."
+  rpcTimeout: number | null; // when the RPCs are tested they are raced, this is the max time to allow for a response
+  proxySettings: ProxySettings; // settings for the proxy
 };
 
 export type NetworkRPCs = typeof networkRpcs;

ubiquity/rpc-handler

types/handler.ts
@@ -1,7 +1,7 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
 import { networkCurrencies, networkExplorers, networkRpcs } from "./constants";
 import { CHAINS_IDS, EXTRA_RPCS } from "./dynamic";
-import { PrettyLogs, PrettyLogsWithOk } from "./logs";
+import { LogInterface, PrettyLogs, PrettyLogsWithOk } from "./logs";
 
 export type BlockExplorer = {
   name: string;
@@ -39,28 +39,30 @@ export type HandlerInterface = {
   testRpcPerformance(): Promise<JsonRpcProvider | null>;
 };
 
-type ModuleName = "[RPCHandler Provider Proxy] -> ";
+// This is log message prefix which can be used to identify the logs from this module
+type ModuleName = "[RPCHandler Provider Proxy] - ";
 
 type ProxySettings = {
-  retryCount: number;
-  retryDelay: number;
+  retryCount: number; // how many times we'll loop the list of RPCs retrying the request before failing
+  retryDelay: number; // how long we'll wait before moving to the next RPC
   // eslint-disable-next-line @typescript-eslint/ban-types
-  logTier: (PrettyLogsWithOk & {}) | null;
-  logger: PrettyLogs | null;
-  strictLogs: boolean;
-  moduleName?: ModuleName | string;
+  logTier: (PrettyLogsWithOk & {}) | null; // set to "none" for no logs, null will default to "error", "verbose" will log all
+  logger: PrettyLogs | LogInterface | null; // null will default to PrettyLogs, otherwise pass in your own logger
+  strictLogs: boolean; // true is default, only the specified logTier will be logged. false will log all logs.
+  moduleName?: ModuleName | string; // this is the prefix for the logs
+  disabled?: boolean;
 };
 
 export type HandlerConstructorConfig = {
   networkId: NetworkId;
   networkName: NetworkName | null;
-  networkRpcs: Rpc[] | null;
-  autoStorage: boolean | null;
-  cacheRefreshCycles: number | null;
-  runtimeRpcs: string[] | null;
-  rpcTimeout: number | null;
-  tracking?: Tracking;
-  proxySettings: ProxySettings;
+  tracking?: Tracking; // "yes" | "limited" | "none", default is "yes". This is the data tracking status of the RPC provider
+  networkRpcs: Rpc[] | null; // e.g "https://mainnet.infura.io/..."
+  autoStorage: boolean | null; // browser only, will store in localStorage
+  cacheRefreshCycles: number | null; // bad RPCs are excluded if they fail, this is how many cycles before they're re-tested
+  runtimeRpcs: string[] | null; // e.g "<networkId>__https://mainnet.infura.io/..." > "1__https://mainnet.infura.io/..."
+  rpcTimeout: number | null; // when the RPCs are tested they are raced, this is the max time to allow for a response
+  proxySettings: ProxySettings; // settings for the proxy
 };
 
 export type NetworkRPCs = typeof networkRpcs;