15b3a42e914374207af06e4d186408ee8187d480
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-07-27 19:54:49 +0900

    chore: fixed knip

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["src/index.ts", "src/services/*.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/types/config.ts", "tests/*"],
+  ignore: ["src/types/config.ts", "tests/*", ".github/workflows/**"],
   ignoreExportsUsedInFile: true,
   ignoreDependencies: ["axios", "ts-node", "node-fetch", "eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@types/node-fetch"],
   jest: {
