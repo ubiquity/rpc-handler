f39fbd9f25358ef1637a5dce1eac64534d36f8e2
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-28 17:05:28 +0000

    test: enhance consensus tests with improved RPC configuration and payload

--- tests/consensus.test.ts
+++ tests/consensus.test.ts
@@ -10,15 +10,16 @@ import nock from "nock";
  * The consensus function is naturally fragile when it comes
  * to CI.
  */
+const rpcUrls = ["http://127.0.0.1:8545", "http://127.0.0.1:8546", "http://127.0.0.1:8547"];
 
 export const testConfig: HandlerConstructorConfig = {
   networkId: "100",
   autoStorage: false,
   cacheRefreshCycles: 3,
   networkName: null,
-  networkRpcs: null,
-  rpcTimeout: 600,
-  runtimeRpcs: null,
+  rpcTimeout: 10000,
+  runtimeRpcs: rpcUrls,
+  networkRpcs: rpcUrls.map((url) => ({ url })),
   proxySettings: {
     retryCount: 3,
     retryDelay: 10,
@@ -28,6 +29,16 @@ export const testConfig: HandlerConstructorConfig = {
   },
 };
 
+const testPayload = {
+  jsonrpc: "2.0",
+  method: "eth_getBlockByNumber",
+  params: ["latest", false],
+  id: 1,
+  headers: {
+    "Content-Type": "application/json",
+  },
+}
+
 describe("Consensus Call", () => {
   afterEach(() => {
     jest.clearAllMocks();
@@ -36,12 +47,9 @@ describe("Consensus Call", () => {
 
   it("Should reach consensus", async () => {
     const module = await import("../types/rpc-handler");
-    const rpcUrls = ["http://127.0.0.1:8545", "http://127.0.0.1:8546", "http://127.0.0.1:8547"];
     const rpcHandler = new module.RPCHandler({
       ...testConfig,
       proxySettings: { ...testConfig.proxySettings, logTier: "verbose" },
-      rpcTimeout: 10000,
-      networkId: "100",
       runtimeRpcs: rpcUrls,
       networkRpcs: rpcUrls.map((url) => ({ url })),
     });
@@ -70,19 +78,41 @@ describe("Consensus Call", () => {
         id: 1,
       });
 
-    const consensus = await rpcHandler.consensusCall(
-      {
-        jsonrpc: "2.0",
-        method: "eth_getBlockByNumber",
-        params: ["latest", false],
-        id: 1,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-      "0.5"
-    );
-
+    const consensus = await rpcHandler.consensusCall(testPayload, "0.5");
     expect(consensus).toBeDefined();
+  }, 15000);
+
+  it("Should fail to reach consensus", async () => {
+    const module = await import("../types/rpc-handler");
+    const rpcHandler = new module.RPCHandler({
+      ...testConfig,
+      proxySettings: { ...testConfig.proxySettings, logTier: "verbose" },
+      rpcTimeout: 10000,
+      networkId: "100",
+    });
+
+    nock(rpcUrls[0])
+      .post("/")
+      .reply(200, {
+        jsonrpc: "2.0",
+        result: { number: "0x1b4", hash: "0x1b4" },
+        id: 1,
+      });
+    nock(rpcUrls[1])
+      .post("/")
+      .reply(200, {
+        jsonrpc: "2.0",
+        result: { number: "0x01", hash: "0x01" },
+        id: 1,
+      });
+    nock(rpcUrls[2])
+      .post("/")
+      .reply(200, {
+        jsonrpc: "2.0",
+        result: { number: "0x", hash: "0x" },
+        id: 1,
+      });
+
+    await expect(rpcHandler.consensusCall(testPayload, "0.5")).rejects.toThrowError();
   }, 15000);
 });
