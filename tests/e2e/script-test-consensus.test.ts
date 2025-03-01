3938e09a2707d9be766c009e87440aa254641883
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-03-01 15:52:50 +0000

    test: add end-to-end tests for RPCHandler functionality

---
+++ tests/e2e/script-test-consensus.test.ts
@@ -0,0 +1,47 @@
+import { RPCHandler } from "../../dist";
+import { RequestPayload } from "../../dist/types/rpc-service";
+import { e2eConfig } from "./config";
+
+describe("RPCHandler E2E Tests", () => {
+  let handler: RPCHandler;
+
+  beforeAll(() => {
+    handler = new RPCHandler(e2eConfig);
+  });
+
+  it("should get block consensus response", async () => {
+    const blockByNumberPayload: RequestPayload = {
+      jsonrpc: "2.0",
+      method: "eth_getBlockByNumber",
+      params: ["latest", false],
+      id: 1,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const blockConsensusResponse = await handler.security.consensusCall(blockByNumberPayload, "0.33"); // 33% to reduce flakiness for CI
+    expect(blockConsensusResponse).toBeDefined();
+    expect(blockConsensusResponse).toHaveProperty("baseFeePerGas");
+    expect(blockConsensusResponse).toHaveProperty("blobGasUsed");
+    expect(blockConsensusResponse).toHaveProperty("logsBloom");
+  }, 15000);
+
+  it("should get transaction consensus response", async () => {
+    const transactionReceiptPayload: RequestPayload = {
+      jsonrpc: "2.0",
+      method: "eth_getTransactionReceipt",
+      params: ["0xa49258109b0b89a6fdcf2367c6465842c785e167d8a5f57a88039fdc66bd513c"],
+      id: 1,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const transactionConsensusResponse = await handler.security.consensusCall(transactionReceiptPayload, "0.33"); // 33% to reduce flakiness for CI
+    expect(transactionConsensusResponse).toBeDefined();
+    expect(transactionConsensusResponse).toHaveProperty("blockHash");
+    expect(transactionConsensusResponse).toHaveProperty("blockNumber");
+    expect((transactionConsensusResponse as { from: string }).from).toEqual("0xae5d1f192013db889b1e2115a370ab133f359765");
+  }, 15000);
+});
---
+++ tests/e2e/script-test.test.ts
@@ -0,0 +1,30 @@
+import { RPCHandler } from "../../dist";
+import { e2eConfig } from "./config";
+
+describe("RPCHandler E2E Tests", () => {
+  let handler: RPCHandler;
+
+  beforeAll(async () => {
+    handler = new RPCHandler(e2eConfig);
+    await handler.getFastestRpcProvider();
+  }, 15000);
+
+  it("should get the fastest RPC provider", async () => {
+    const provider = await handler.getFastestRpcProvider();
+    expect(provider).toBeDefined();
+  }, 15000);
+
+  it("should return non-null latencies", async () => {
+    const latencies = handler.getLatencies();
+    expect(latencies).toBeDefined();
+    expect(Object.keys(latencies).length).toBeGreaterThan(0);
+    expect(Object.values(latencies).every((latency) => !!latency && latency > 0)).toBeTruthy();
+  }, 15000);
+
+  it("should return non-null network RPCs", async () => {
+    const networkRpcs = handler.getNetworkRpcs();
+    expect(networkRpcs).toBeDefined();
+    expect(networkRpcs.length).toBeGreaterThan(0);
+    expect(networkRpcs.every((rpc) => !!rpc.url)).toBeTruthy();
+  }, 15000);
+});
